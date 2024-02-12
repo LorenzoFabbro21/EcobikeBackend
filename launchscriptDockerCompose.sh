c15b43fa1cc679bf2a9c2aa002f3a1e84e874c4c
LorenzoFabbro21 <fabbro.lorenzo@edu.unito.it>
2024-02-12 15:42:23 +0100

    wip

---
+++ launchscriptDockerCompose.sh
@@ -0,0 +1,13 @@
+
+cd service-discovery-server
+docker build -t service-discovery-server .
+
+cd ../api-gateway
+docker build -t api-gateway .
+
+cd ../user-service
+docker build -t user-service .
+
+cd ../recensione-service
+docker build -t recensione-service .
+cd ..
